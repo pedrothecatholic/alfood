65099e72d655d0cadc19295c50b9109b4e85f66a
Pedro <pedro@pop-os.localdomain>
2023-03-06 12:24:09 -0300

    Primeiro commit do curso de integração de API no React

--- src/paginas/VitrineRestaurantes/index.tsx
+++ src/paginas/VitrineRestaurantes/index.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
-import Banner from '../../componentes/Banner';
-import ListaRestaurantes from '../../componentes/ListaRestaurantes';
-import NavBar from '../../componentes/NavBar';
-import Rodape from '../../componentes/Rodape';
+import Banner from "../../componentes/Banner";
+import ListaRestaurantes from "../../componentes/ListaRestaurantes";
+import NavBar from "../../componentes/NavBar";
+import Rodape from "../../componentes/Rodape";
 
 function App() {
   return (
