098ad74411afd7a0e2284139f4365b08b1c67c90
pedrothecatholic <pedro.sousa21@outlook.com>
2023-03-07 12:48:57 -0300

    Adicionando o 1º Desafio feito!

--- src/componentes/ListaRestaurantes/index.tsx
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -2,49 +2,54 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { IPaginacao } from "../../interfaces/IPaginacao";
 import IRestaurante from "../../interfaces/IRestaurante";
-import style from "./ListaRestaurantes.module.scss";
+import estilos from "./ListaRestaurantes.module.scss";
 import Restaurante from "./Restaurante";
 
 const ListaRestaurantes = () => {
   const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([]);
   const [proximaPagina, setProximaPagina] = useState("");
+  const [paginaAnterior, setPaginaAnterior] = useState("");
 
-  useEffect(() => {
-    // Obter restaurantes
+  const carregarDados = (url: string) => {
     axios
-      .get<IPaginacao<IRestaurante>>(
-        "http://localhost:8000/api/v1/restaurantes/"
-      )
+      .get<IPaginacao<IRestaurante>>(url)
       .then((resposta) => {
         setRestaurantes(resposta.data.results);
         setProximaPagina(resposta.data.next);
-      })
-      .catch((erro) => {
-        console.log(erro);
-      });
-  }, []);
-
-  const verMais = () => {
-    axios
-      .get<IPaginacao<IRestaurante>>(proximaPagina)
-      .then((resposta) => {
-        setRestaurantes([...restaurantes, ...resposta.data.results]);
-        setProximaPagina(resposta.data.next);
+        setPaginaAnterior(resposta.data.previous);
       })
       .catch((erro) => {
         console.log(erro);
       });
   };
 
+  useEffect(() => {
+    // obter restaurantes
+    carregarDados("http://localhost:8000/api/v1/restaurantes/");
+  }, []);
+
   return (
-    <section className={style.ListaRestaurantes}>
+    <section className={estilos.ListaRestaurantes}>
       <h1>
-        Os restaurantes mais <em>bacanas</em>!
+        Os restaurantes mais <em>bacanas </em>!
       </h1>
       {restaurantes?.map((item) => (
         <Restaurante restaurante={item} key={item.id} />
       ))}
-      {proximaPagina && <button onClick={verMais}>Ver Mais</button>}
+      <div className={estilos.ContainerBotoes}>
+        <button
+          className={estilos.Botoes}
+          onClick={() => carregarDados(paginaAnterior)}
+          disabled={!paginaAnterior}>
+          Página Anterior
+        </button>
+        <button
+          className={estilos.Botoes}
+          onClick={() => carregarDados(proximaPagina)}
+          disabled={!proximaPagina}>
+          Próxima página
+        </button>
+      </div>
     </section>
   );
 };
